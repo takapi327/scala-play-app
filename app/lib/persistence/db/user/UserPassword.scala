25be4df5f4de70ad5dfc8ce2529425707ffb987d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-05-10 12:47:53 +0900

    Create UserPasswordTable Model

---
+++ app/lib/persistence/db/user/UserPassword.scala
@@ -0,0 +1,28 @@
+package lib.persistence.db
+
+import java.time.LocalDateTime
+import slick.jdbc.JdbcProfile
+import play.api.db.slick.DatabaseConfigProvider
+import lib.model.UserPassword
+
+case class UserPassTable()
+  (dbConfigProvider: DatabaseConfigProvider){
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import profile.api._
+  import lib.model.{UserPassword => Pass}
+
+  private class UserPassTable(tag: Tag) extends Table[UserPassword](tag, "userPassword"){
+    def id            = column[Option[Pass.Id]]  ("id", O.PrimaryKey, O.AutoInc)
+    def password      = column[String]           ("password")
+    def repassword    = column[String]           ("repassword")
+    def updatedAt     = column[LocalDateTime]    ("updated_at")
+    def createdAt     = column[LocalDateTime]    ("created_at")
+
+    def * = (id, password, repassword, updatedAt, createdAt) <> (
+      (UserPassword.apply _).tupled, UserPassword.unapply
+    )
+  }
+}
