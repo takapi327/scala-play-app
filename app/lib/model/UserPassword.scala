a1b2aa418c33c0308f4d5ff123cfd187ebae0c82
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-05-10 11:32:59 +0900

    Create User&Password Model

---
+++ app/lib/model/UserPassword.scala
@@ -0,0 +1,21 @@
+package lib.model
+
+import java.time.LocalDateTime
+import java.security.Identity
+
+import UserPassword._
+
+case class UserPassword (
+  id:          Option[Id],
+  password:    String,
+  repassword:  String,
+  updateAt:    LocalDateTime,
+  createdAt:   LocalDateTime
+)
+
+object UserPassword {
+
+  type Id = Long
+
+  case class FormValue (password: String, repassword: String)
+}
