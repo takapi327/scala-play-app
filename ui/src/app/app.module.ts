a8bf23e72e0aafbfffeca1f8bf33f06f8c926e15
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-08-11 09:04:21 +0900

    Create SignUp Component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 // ---- [ Component ] --------------------------------------
 import { AppComponent }     from './app.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
