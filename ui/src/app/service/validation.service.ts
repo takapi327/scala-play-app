768bf0f4e754bad882744bf7ec0d56d811bd6e95
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-08-21 23:38:47 +0900

    Create Password Validation

--- ui/src/app/service/validation.service.ts
+++ ui/src/app/service/validation.service.ts
@@ -8,13 +8,33 @@ export class ValidationService {
 
   constructor() { }
 
+  // ---- [ variable ] -------------
+  /**
+   *
+   * メールアドレスのバリデーション
+   * @export Email Login Signup Component
+   */
   email = new FormControl('', [
     Validators.required,
     Validators.email
   ]);
   
+  /**
+   *
+   * パスワードのバリデーション
+   * @export password Login Signup Component
+   */
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8)
+  ]);
+
+  // ---- [ method ] ----------------
   isEmailValidation(): FormControl {
     return this.email
   }
   
+  isPasswordValidation(): FormControl {
+    return this.password
+  }
 }
