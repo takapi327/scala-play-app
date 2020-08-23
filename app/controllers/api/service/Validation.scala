2559ee8013ba16106328c5e57676aba34e9e1f9b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-08-23 21:45:59 +0900

    Create ValidationController emailRegistered Action

--- app/controllers/api/service/Validation.scala
+++ app/controllers/api/service/Validation.scala
@@ -3,16 +3,22 @@ package controllers.api.service
 import javax.inject.Inject
 import scala.concurrent.{Future, ExecutionContext}
 
-import json.reads.JsValueReadsUser
-import json.writes.JsValueWritesIsAuth
+import json.reads.JsValueReadsEmail
+import json.writes.{JsValueWritesIsAuth, JsValueWritesEmailValid}
+
+import cats.data.EitherT
+import cats.implicits._
 
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.i18n.I18nSupport
 import play.api.libs.json.JsPath.json
 
+import lib.persistence.UserRepository
+
 class ValidationController @Inject()(
-  cc: MessagesControllerComponents
+  uerRepo: UserRepository,
+  cc:      MessagesControllerComponents
 )(implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc)
   with    I18nSupport {
@@ -31,7 +37,15 @@ class ValidationController @Inject()(
     }
   }
 
-  def emailValidation() = Action(parse.json) async {implicit request =>
-    ???
+  def isEmailRegistered() = Action(parse.json) async {implicit request =>
+    val jsonEmail = request.body.validate[JsValueReadsEmail].get
+    for {
+      email <- uerRepo.filterByMail(jsonEmail.email)
+    } yield {
+      email match {
+        case Some(_) => Ok(Json.toJson(JsValueWritesEmailValid(isRegistered = true)))
+        case None    => Ok(Json.toJson(JsValueWritesEmailValid(isRegistered = false)))
+      }
+    }
   }
 }
