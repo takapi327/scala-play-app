b109ad10afb13591f413d6cffe53451861a259a7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-08-31 23:05:35 +0900

    Fixed auth.module

--- ui/src/app/auth/auth.module.ts
+++ ui/src/app/auth/auth.module.ts
@@ -5,11 +5,12 @@ import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // ---- [ Component ] -----------------------------------------------
-import { LoginComponent }    from './login/login.component';
-import { SignupComponent }   from './signup/signup.component';
-import { LogoutComponent }   from './logout/logout.component';
-import { EmailComponent }    from './email/email.component';
-import { PasswordComponent } from './password/password.component';
+import { LoginComponent }     from './login/login.component';
+import { SignupComponent }    from './signup/signup.component';
+import { LogoutComponent }    from './logout/logout.component';
+import { EmailComponent }     from './email/email.component';
+import { PasswordComponent }  from './password/password.component';
+import { FirstchatComponent } from '../auth/chatForm/firstchat/firstchat.component';
 
 // ---- [ Cookie ] -----------------------------------------------
 import { CookieService } from 'ngx-cookie-service';
@@ -20,7 +21,8 @@ import { CookieService } from 'ngx-cookie-service';
     SignupComponent,
     LogoutComponent,
     EmailComponent,
-    PasswordComponent
+    PasswordComponent,
+    FirstchatComponent
   ],
   imports: [
     CommonModule,
