6564985eece49822a29894364668b14a69887076
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-01 00:40:03 +0900

    Create SecondChatForm

---
+++ ui/src/app/auth/chatForm/secondchat/secondchat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondchat',
+  templateUrl: './secondchat.component.html',
+  styleUrls: ['./secondchat.component.scss']
+})
+export class SecondchatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
