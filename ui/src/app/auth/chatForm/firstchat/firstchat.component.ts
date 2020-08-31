49901edc8613c17678e4f032721d543dd10de181
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-08-31 23:04:48 +0900

    Create chat form Directory

---
+++ ui/src/app/auth/chatForm/firstchat/firstchat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstchat',
+  templateUrl: './firstchat.component.html',
+  styleUrls: ['./firstchat.component.scss']
+})
+export class FirstchatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
