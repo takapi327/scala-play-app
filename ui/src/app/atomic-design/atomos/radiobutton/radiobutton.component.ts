7ee07b957de3762f924619abb99d6486a3649850
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-13 15:34:47 +0900

    Fixed radiobutton.component

--- ui/src/app/atomic-design/atomos/radiobutton/radiobutton.component.ts
+++ ui/src/app/atomic-design/atomos/radiobutton/radiobutton.component.ts
@@ -3,7 +3,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector:    'app-radiobutton',
   templateUrl: './radiobutton.component.html',
-  styleUrls:   ['./radiobutton.component.scss']
+  styleUrls:   [
+    './radiobutton.component.scss',
+    '../../molecules/radio/radio.component.scss'
+  ],
 })
 export class RadiobuttonComponent {
 
@@ -16,8 +19,7 @@ export class RadiobuttonComponent {
 
   @Output() eventValue = new EventEmitter();
 
-  constructor() {
-  }
+  constructor() {}
 
   showValue(index: number, value: string) {
     this.eventValue.emit({index, value});
