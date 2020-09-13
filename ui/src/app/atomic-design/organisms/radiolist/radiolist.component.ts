11fc7c3c8bb3174c38fa060249e981804b88a3cb
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-13 14:28:41 +0900

    Create organisms

---
+++ ui/src/app/atomic-design/organisms/radiolist/radiolist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector:    'app-radiolist',
+  templateUrl: './radiolist.component.html',
+  styleUrls:   ['./radiolist.component.scss']
+})
+export class RadiolistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
