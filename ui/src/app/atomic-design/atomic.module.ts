7a38ac95e1f5d5f903729a35e0ba3fe6832f4ccb
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-19 12:29:48 +0900

    comment out organisms

--- ui/src/app/atomic-design/atomic.module.ts
+++ ui/src/app/atomic-design/atomic.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }             from '@angular/core';
 import { CommonModule }         from '@angular/common';
-import { FormsModule }          from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }          from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CheckboxComponent }    from './molecules/checkbox/checkbox.component';
@@ -18,6 +18,7 @@ const COMPONENTS = [
 const MODULES = [
   CommonModule,
   FormsModule,
+  ReactiveFormsModule,
   BrowserAnimationsModule
 ]
 
