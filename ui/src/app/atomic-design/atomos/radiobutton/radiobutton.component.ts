a587546b0dd25e3348faf475944d0ad13e2df412
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-20 15:36:52 +0900

    Fixed RadioComponent CustomValueAccessor

--- ui/src/app/atomic-design/atomos/radiobutton/radiobutton.component.ts
+++ ui/src/app/atomic-design/atomos/radiobutton/radiobutton.component.ts
@@ -9,19 +9,11 @@ import { ControlValueAccessorConnector } from '../../control-value-accessor-conn
   selector:    'app-radiobutton',
   templateUrl: './radiobutton.component.html',
   styleUrls:   ['./radiobutton.component.scss'],
-  providers:   [
-    {
-      provide:     NG_VALUE_ACCESSOR,
-      useExisting: RadiobuttonComponent,
-      multi:       true
-    }
-  /*,
-  {
-    provide: NgControl,
-    useExisting: forwardRef(() => ControlValueAccessorConnector)
-  }
-  */
-  ]
+  providers:   [{
+    provide:     NG_VALUE_ACCESSOR,
+    useExisting: RadiobuttonComponent,
+    multi:       true
+  }]
 })
 export class RadiobuttonComponent extends ControlValueAccessorConnector {
 
@@ -30,33 +22,12 @@ export class RadiobuttonComponent extends ControlValueAccessorConnector {
 
   @Output() selectLocation = new EventEmitter();
 
-  //selected:   string;
-  //disabled: boolean;
-  //@ViewChild('input') private inputElementRef: ElementRef;
-
   constructor(
     injector: Injector
-    //@Self() public controlDir: NgControl
   ) {
     super(injector);
-    console.log(this.formControlDirective)
-    //controlDir.valueAccessor = this;
   }
-/*
-  ngOnInit(){
-    this.radioValue = this.radioOptions
-    console.log(this.radioOptions)
-    const control = this.controlDir.control;
-    control.setValidators(this.validate);
-    control.updateValueAndValidity();
-  }
-    */
 
-  /*
-  validate(ctrl: AbstractControl){
-    return Validators.required(ctrl);
-  }
-  */
   /**
    * ===== Method =====
    */
@@ -65,25 +36,4 @@ export class RadiobuttonComponent extends ControlValueAccessorConnector {
     this.selectLocation.emit({index, value});
   }
 
-  //onTouched: (obj: any) => void;
-  /**
-   * ===== ControlValueAccessor =====
-   */
-  /*
-  writeValue(obj: any): void {
-    this.inputElementRef = obj;
-  }
-
-  registerOnChange(fn: any): void {
-    this.onChange
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn
-  }
-
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled
-  }
-  */
 }
