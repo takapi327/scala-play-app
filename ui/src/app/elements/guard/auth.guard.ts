86b711924d08ba94a7b452bbaad55e56181478c2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-21 13:26:24 +0900

    File Change Gard Directory

--- ui/src/app/elements/guard/auth.guard.ts
+++ ui/src/app/elements/guard/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable }          from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 import { AuthService } from '../service/auth.service';
-import { Auth }        from '../interface/user'
+import { Auth }        from '../../interface/user'
 
 @Injectable({
   providedIn: 'root'
