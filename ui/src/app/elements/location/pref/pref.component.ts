79105197590bf9b8e48ec1245cf9eeeca7573b43
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-30 00:41:10 +0900

    create area pref component

---
+++ ui/src/app/elements/location/pref/pref.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pref',
+  templateUrl: './pref.component.html',
+  styleUrls: ['./pref.component.scss']
+})
+export class PrefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
